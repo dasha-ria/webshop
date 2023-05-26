da02536c459b42dcbe3bbe2712a962ff5e5f4564
Daria <dariastoicad@gmail.com>
2023-05-26 22:56:44 +0200

    more on cart popup

--- src/components/shopping-cart.tsx
+++ src/components/shopping-cart.tsx
@@ -1,10 +1,21 @@
-export const ShoppingCart = ({ cartPopup, setCartPopup }) => {
+import { useShoppingCart } from "@/context/ShoppingCartContext";
+
+export const ShoppingCart = ({ cartPopup, setCartPopup, id, quantity }) => {
+  const { closeCart, cartItems, removeFromCart } = useShoppingCart();
   const closeCartPopup = () => setCartPopup(false);
+  console.log(cartItems);
   return (
     <div className="flex justify-end pr-10">
-      <div className="flex flex-row gap-40 mt-5">
+      <div className="flex flex-row gap-40 mt-5 items-center justify-center">
         <p>Cart</p>
-        <button onClick={closeCartPopup}>close</button>
+        <button onClick={(closeCart, closeCartPopup)}>
+          <img className="w-6 h-6" src="close.svg"></img>
+        </button>
+      </div>
+      <div>
+        {cartItems.map((item) => (
+          <div key={item.id} {...item}></div>
+        ))}
       </div>
     </div>
   );
