d8e8d725fe5c9ce3ba35b6dec95962f3cd942e6e
Daria <dariastoicad@gmail.com>
2023-06-01 13:45:50 +0200

    condition

--- src/components/shopping-cart.tsx
+++ src/components/shopping-cart.tsx
@@ -7,49 +7,52 @@ export const ShoppingCart = ({ clothing }) => {
 
   console.log(cartItems);
   return (
-    <div className="flex flex-col justify-end p-4 gap-4 absolute top-24 right-10 border border-black bg-white shadow-md rounded-md">
-      <div className="flex flex-row gap-40 mt-5 items-center justify-center">
-        <p>Cart</p>
+    <div className="flex flex-col justify-end p-4 gap-4 absolute top-24 right-10 border border-black bg-white shadow-md rounded-md w-96">
+      <div className="flex justify-end">
         <button onClick={closeCart}>
           <img className="w-6 h-6" src="close.svg"></img>
         </button>
       </div>
       <div className="flex flex-col gap-3 mt-5 items-center justify-center">
-        {cartItems.map((item) => {
-          const clothingData = clothing.find((data) => data.id === item.id);
-          return (
-            <div className="flex flex-row gap-14" key={item.id} {...item}>
-              <div className="flex flex-row gap-4">
-                <div>
-                  <img
-                    className="w-20 h-20 object-cover rounded-md"
-                    src={clothingData.url}
-                  ></img>
+        {cartItems.length === 0 ? (
+          <p className="mb-8">Your cart is empty</p>
+        ) : (
+          cartItems.map((item) => {
+            const clothingData = clothing.find((data) => data.id === item.id);
+            return (
+              <div className="flex flex-row gap-14" key={item.id} {...item}>
+                <div className="flex flex-row gap-4">
+                  <div>
+                    <img
+                      className="w-20 h-20 object-cover rounded-md"
+                      src={clothingData.url}
+                    ></img>
+                  </div>
+                  <div>
+                    <p>{clothingData.name}</p>
+                    <PriceWithoutOriginalPrice
+                      price={clothingData.price}
+                      discount={clothingData.discount}
+                    ></PriceWithoutOriginalPrice>
+                  </div>
                 </div>
                 <div>
-                  <p>{clothingData.name}</p>
-                  <PriceWithoutOriginalPrice
-                    price={clothingData.price}
-                    discount={clothingData.discount}
-                  ></PriceWithoutOriginalPrice>
+                  $
+                  <span>
+                    {getPrice(clothingData.price, clothingData.discount) *
+                      item.quantity}
+                  </span>
+                </div>
+                <div
+                  className="cursor-pointer"
+                  onClick={() => removeFromCart(item.id)}
+                >
+                  <img className="w-5 h-5" src="trash.svg"></img>
                 </div>
               </div>
-              <div>
-                $
-                <span>
-                  {getPrice(clothingData.price, clothingData.discount) *
-                    item.quantity}
-                </span>
-              </div>
-              <div
-                className="cursor-pointer"
-                onClick={() => removeFromCart(item.id)}
-              >
-                <img className="w-5 h-5" src="trash.svg"></img>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })
+        )}
       </div>
     </div>
   );
